cbaf685b1847def1eb8cc9dd61c372ad6b0091b9
Ceratt1 <gabrielceratticabral@gmail.com.br>
2025-03-13 19:47:28 -0300

    test: mock next/image in home.test.tsx for improved image rendering tests

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -2,15 +2,27 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "@/app/page";
 import Newsletter from "@/components/sections/newslatter";
 
+
+jest.mock('next/image', () => {
+    const Image = ({ src, alt, width, height }: { src: string, alt: string, width: number, height: number }) => {
+      return <img src={src} alt={alt} width={width} height={height} />;
+    };
+    Image.displayName = 'MockedImage';
+    return Image;
+  });
+
+
 describe("Home Page", () => {
     it("teste para verificar se estou conseguindo renderizar as imagens", () => {
+
         render(<Home />);
 
         const images = screen.getAllByRole('img');
+
         images.forEach((image) => {
-            expect(image).toBeInTheDocument();
-            expect(image).toHaveAttribute('src');
-        });
+        expect(image).toBeInTheDocument();
+        expect(image).toHaveAttribute('src');
+    });
     });
 
     it("teste para verificar se estou conseguindo preencher os inputs", () => {
